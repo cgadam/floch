7aba4c8e0089cb91d718e58ddcfeb59418724c34
Christian Adam <christian.g.adam@gmail.com>
2019-01-14 01:14:14 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/floch-app/precache-manifest.7c1927cc36174b0f6216c2703c86cd63.js"
+  "/floch/precache-manifest.5720b5fa22bee5c28c3da92c361f423e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/floch-app/index.html", {
+workbox.routing.registerNavigationRoute("/floch/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
