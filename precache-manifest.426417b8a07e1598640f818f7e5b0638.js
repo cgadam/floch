13ce4245f0d0faf94035b039d925f2503deae340
Christian Adam <christian.g.adam@gmail.com>
2019-01-14 00:49:20 -0300

    Initial dist subtree commit

---
+++ precache-manifest.426417b8a07e1598640f818f7e5b0638.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bcb6d44730fc8d29abbbb105e4d87d4",
+    "url": "/static/media/lamerced.8bcb6d44.png"
+  },
+  {
+    "revision": "0a12f9584af0b6134906",
+    "url": "/static/css/main.202d74b9.chunk.css"
+  },
+  {
+    "revision": "b38d3faf16ee4e25602bbf76336b90c9",
+    "url": "/static/media/handycheera.b38d3faf.otf"
+  },
+  {
+    "revision": "70b002ac679a003041ef",
+    "url": "/static/js/1.70b002ac.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "3cb71f23c1cda8fa7e86a986ba23f263",
+    "url": "/static/media/marquesina.3cb71f23.png"
+  },
+  {
+    "revision": "0a12f9584af0b6134906",
+    "url": "/static/js/main.0a12f958.chunk.js"
+  },
+  {
+    "revision": "f71e2d8b1949d4d70ff6a3cd29d3e3aa",
+    "url": "/static/media/rings.f71e2d8b.png"
+  },
+  {
+    "revision": "ac710b068e6ca8accf884ddc4470d16c",
+    "url": "/static/media/lacautiva.ac710b06.png"
+  },
+  {
+    "revision": "0ef4294debf8778a5ea52aeb764567c3",
+    "url": "/static/media/gmaps.0ef4294d.png"
+  },
+  {
+    "revision": "e93e153b568e717af52f967f533f6643",
+    "url": "/static/media/cashInHand.e93e153b.png"
+  },
+  {
+    "revision": "cfacbe9e67aeef67c7b572d0fe8baf63",
+    "url": "/static/media/11b_treinta_chris.cfacbe9e.jpg"
+  },
+  {
+    "revision": "88a9003c5f0b1a83323141c78a806567",
+    "url": "/static/media/14_cuba_02.88a9003c.jpg"
+  },
+  {
+    "revision": "5c174d73cdf51c022a34d38f1bf4084c",
+    "url": "/static/media/Typey.5c174d73.otf"
+  },
+  {
+    "revision": "70b002ac679a003041ef",
+    "url": "/static/css/1.9fb07b82.chunk.css"
+  },
+  {
+    "revision": "c53f3f2670f29c8d7f90a32d495f25b0",
+    "url": "/index.html"
+  }
+];
