8a72c0fce4affd575723b1e3e6c77357e17a7c8a
Christian Adam <christian.g.adam@gmail.com>
2019-01-14 01:09:14 -0300

    Build

---
+++ build/precache-manifest.6d49f912035891d6af260b6a93066667.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bcb6d44730fc8d29abbbb105e4d87d4",
+    "url": "/floch-app/static/media/lamerced.8bcb6d44.png"
+  },
+  {
+    "revision": "aac9d7643bc3699e827d",
+    "url": "/floch-app/static/css/main.3e06cb39.chunk.css"
+  },
+  {
+    "revision": "b38d3faf16ee4e25602bbf76336b90c9",
+    "url": "/floch-app/static/media/handycheera.b38d3faf.otf"
+  },
+  {
+    "revision": "e94b20b3bbbbfaf6fe38",
+    "url": "/floch-app/static/js/1.e94b20b3.chunk.js"
+  },
+  {
+    "revision": "45e484c5567dd3f1b663",
+    "url": "/floch-app/static/js/runtime~main.45e484c5.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/floch-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/floch-app/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/floch-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/floch-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "3cb71f23c1cda8fa7e86a986ba23f263",
+    "url": "/floch-app/static/media/marquesina.3cb71f23.png"
+  },
+  {
+    "revision": "aac9d7643bc3699e827d",
+    "url": "/floch-app/static/js/main.aac9d764.chunk.js"
+  },
+  {
+    "revision": "f71e2d8b1949d4d70ff6a3cd29d3e3aa",
+    "url": "/floch-app/static/media/rings.f71e2d8b.png"
+  },
+  {
+    "revision": "ac710b068e6ca8accf884ddc4470d16c",
+    "url": "/floch-app/static/media/lacautiva.ac710b06.png"
+  },
+  {
+    "revision": "0ef4294debf8778a5ea52aeb764567c3",
+    "url": "/floch-app/static/media/gmaps.0ef4294d.png"
+  },
+  {
+    "revision": "e93e153b568e717af52f967f533f6643",
+    "url": "/floch-app/static/media/cashInHand.e93e153b.png"
+  },
+  {
+    "revision": "cfacbe9e67aeef67c7b572d0fe8baf63",
+    "url": "/floch-app/static/media/11b_treinta_chris.cfacbe9e.jpg"
+  },
+  {
+    "revision": "88a9003c5f0b1a83323141c78a806567",
+    "url": "/floch-app/static/media/14_cuba_02.88a9003c.jpg"
+  },
+  {
+    "revision": "5c174d73cdf51c022a34d38f1bf4084c",
+    "url": "/floch-app/static/media/Typey.5c174d73.otf"
+  },
+  {
+    "revision": "e94b20b3bbbbfaf6fe38",
+    "url": "/floch-app/static/css/1.7a25411d.chunk.css"
+  },
+  {
+    "revision": "e0ad4eb6f203c4bf9a52fd84e59afcb9",
+    "url": "/floch-app/index.html"
+  }
+];
